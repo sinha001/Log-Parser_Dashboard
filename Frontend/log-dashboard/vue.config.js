7f308db896f820305ee0abb414ca4b0b9b1d1637
sinha001 <sinhasonu004@gmail.com>
2025-02-14 18:13:11 +0530

    commit-1

--- Frontend/log-dashboard/vue.config.js
+++ Frontend/log-dashboard/vue.config.js
@@ -2,11 +2,12 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/',  // Set the public path to '/'
+  publicPath: '/',  // Set publicPath to '/' to serve from the root of the deployed project
+  outputDir: 'dist',  // Make sure the build output is placed correctly in `log-dashboard/dist/`
   configureWebpack: {
     resolve: {
       alias: {
-        'chart.js': 'chart.js/dist/chart.min.js', // Ensure using the minified browser version of chart.js
+        'chart.js': 'chart.js/dist/chart.min.js',
       }
     }
   }
